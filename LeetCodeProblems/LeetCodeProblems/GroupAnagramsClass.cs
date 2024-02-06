26fc7d8ad06f27b6b6c249a27287f75d071adb43
Caitlin Campbell <cacampbell.tech@gmail.com>
2024-02-06 16:11:39 -0600

    Group Anagrams

---
+++ LeetCodeProblems/LeetCodeProblems/GroupAnagramsClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace LeetCodeProblems
+{
+	public class GroupAnagramsClass
+	{
+        public static IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            Dictionary<string, IList<string>> anagramLookup = new Dictionary<string, IList<string>>();
+
+            foreach(string str in strs)
+            {
+                string alphabatizedString = alphabatizeCharacters(str);
+
+                if(!anagramLookup.ContainsKey(alphabatizedString))
+                {
+                    anagramLookup.Add(alphabatizedString, new List<string>());
+                }
+                anagramLookup[alphabatizedString].Add(str);
+            }
+
+            return anagramLookup.Select(kvp => kvp.Value).ToList();
+        }
+
+        private static string alphabatizeCharacters(string str)
+        {
+            return String.Concat(str.OrderBy(c => c));
+        }
+    }
+}
+

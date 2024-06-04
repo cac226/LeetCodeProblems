7109e2c4dff49a2bca5444b1b6e697a83456e9b0
Caitlin Campbell <cacampbell.tech@gmail.com>
2024-06-04 11:37:16 -0500

    longest palindrome draft

--- LeetCodeProblems/LeetCodeProblems/Palindrome.cs
+++ LeetCodeProblems/LeetCodeProblems/Palindrome.cs
@@ -3,6 +3,42 @@ namespace LeetCodeProblems
 {
 	public class Palindrome
 	{
+        /// <summary>
+        /// Given a string s which consists of lowercase or uppercase letters,
+        /// return the length of the longest palindrome that can be built with
+        /// those letters.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static int LongestPalindrome(string s)
+        {
+            if(s.Length <= 1)
+            {
+                return s.Length;
+            }
+
+            string sSorted = String.Concat(s.OrderBy(c => c));
+
+            int charPairCount = 0;
+            bool hasSpareChar = s.Length % 2 == 1;
+
+            int index = 0;
+            while(index < sSorted.Length)
+            {
+                char c = sSorted[index];
+                int lastIndex = sSorted.LastIndexOf(c);
+                int occurances = lastIndex - index + 1;
+                charPairCount += occurances / 2;
+                if(!hasSpareChar && occurances % 2 == 1)
+                {
+                    hasSpareChar = true;
+                }
+                index += occurances;
+            }
+
+            return (charPairCount * 2) + (hasSpareChar ? 1 : 0);
+        }
+
         public static string FirstPalindrome(string[] words)
         {
             foreach(string word in words)
@@ -13,7 +49,7 @@ namespace LeetCodeProblems
                 }
             }
 
-            return "";
+            return ""; // no palindrome found in array, return empty string
         }
 
         private static bool isPalindrome(string str)
